6057a7226845d0d4eef40a8d69ae247d5445bb6f
Martynas Vasilis <martynas.vasilis@gmail.com>
2026-01-01 20:59:30 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const METERS_TO_UNITS = 1 / 1e11; // scale factor
-export const SCENE_SCALE = 200;          // visual multiplier for spacing
-export const DISTANCE_SCALE = SCENE_SCALE * METERS_TO_UNITS; // combined
+export const METERS_TO_UNITS = 1 / 1e11; 
+export const SCENE_SCALE = 200;          
+export const DISTANCE_SCALE = SCENE_SCALE * METERS_TO_UNITS; 
